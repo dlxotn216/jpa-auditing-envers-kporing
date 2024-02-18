8de736827fd252a60c0e71cfe24b9ad2c2480f70
taesu <taesulee93@gmail.com>
2024-02-18 14:46:45 +0900

    Initial project setup

---
+++ src/main/kotlin/io/taesu/jpaauditingenverskporing/JpaAuditingEnversKporingApplication.kt
@@ -0,0 +1,11 @@
+package io.taesu.jpaauditingenverskporing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaAuditingEnversKporingApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaAuditingEnversKporingApplication>(*args)
+}
